b90746034e7ba0aeab114c51c3bd2ec6a0699ff9
kayson0512 <kayson0512@163.com>
2018-04-17 23:48:33 +0800

    workerList init

--- src/pages/workerlist/index.js
+++ src/pages/workerlist/index.js
@@ -1,45 +1,78 @@
-/**
- * Created by hao.cheng on 2017/4/15.
- */
+
 import React from 'react';
-import { Row, Col, Card } from 'antd';
-const BasicTables = () => (
-    <div className="gutter-example">
-        <Row gutter={16}>
-            <Col className="gutter-row" md={24}>
-                <div className="gutter-box">
-                    <Card title="基础表格" bordered={false}>
-                       
-                    </Card>
-                </div>
-            </Col>
-        </Row>
-        <Row gutter={16}>
-            <Col className="gutter-row" md={24}>
-                <div className="gutter-box">
-                    <Card title="基础表格" bordered={false}>
-                    
-                    </Card>
-                </div>
-            </Col>
-        </Row>
-        <Row gutter={16}>
-            <Col className="gutter-row" md={12}>
-                <div className="gutter-box">
-                    <Card title="可控的筛选和排序" bordered={false}>
-                      
-                    </Card>
-                </div>
-            </Col>
-            <Col className="gutter-row" md={12}>
-                <div className="gutter-box">
-                    <Card title="自定义筛选" bordered={false}>
-                    
-                    </Card>
-                </div>
-            </Col>
-        </Row>
-    </div>
+import { Table, Icon } from 'antd';
+
+const columns = [{
+    title: 'Name',
+    dataIndex: 'name',
+    filters: [{
+      text: 'Joe',
+      value: 'Joe',
+    }, {
+      text: 'Jim',
+      value: 'Jim',
+    }, {
+      text: 'Submenu',
+      value: 'Submenu',
+      children: [{
+        text: 'Green',
+        value: 'Green',
+      }, {
+        text: 'Black',
+        value: 'Black',
+      }],
+    }],
+    // specify the condition of filtering result
+    // here is that finding the name started with `value`
+    onFilter: (value, record) => record.name.indexOf(value) === 0,
+    sorter: (a, b) => a.name.length - b.name.length,
+  }, {
+    title: 'Age',
+    dataIndex: 'age',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.age - b.age,
+  }, {
+    title: 'Address',
+    dataIndex: 'address',
+    filters: [{
+      text: 'London',
+      value: 'London',
+    }, {
+      text: 'New York',
+      value: 'New York',
+    }],
+    filterMultiple: false,
+    onFilter: (value, record) => record.address.indexOf(value) === 0,
+    sorter: (a, b) => a.address.length - b.address.length,
+  }];
+  
+  const data = [{
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+  }, {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+  }, {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+  }, {
+    key: '4',
+    name: 'Jim Red',
+    age: 32,
+    address: 'London No. 2 Lake Park',
+  }];
+  function onChange(pagination, filters, sorter) {
+    console.log('params', pagination, filters, sorter);
+  }
+
+const workerList = () => (
+    <Table columns={columns} dataSource={data} onChange={onChange}/>
 );
 
-export default BasicTables;
+export default workerList;

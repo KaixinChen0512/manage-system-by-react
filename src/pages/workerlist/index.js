87d6f2c622a8ebb32e716d7d77b8291ec9bef6b9
kayson0512 <kayson0512@163.com>
2018-04-18 14:29:39 +0800

    page workerList finished

--- src/pages/workerlist/index.js
+++ src/pages/workerlist/index.js
@@ -1,64 +1,106 @@
-
-import React from 'react';
-import { Table, Icon } from 'antd';
-
-const columns = [{
-    title: 'Name',
-    dataIndex: 'name',
-    filters: [{
-      text: 'Joe',
-      value: 'Joe',
+import "./index.css";
+import React,{Component} from 'react';
+import { Table, Icon,Button,Input } from 'antd';
+const data = [];
+for(let i=1;i<=100;i++){
+    data.push({
+        key:i,
+        name:`kayson ${i}`,
+        age:Math.ceil(Math.random()*10)+20,
+        address:`China ${i}`
+    })
+}
+const rowSelection = {
+  onChange: (selectedRowKeys, selectedRows) => {
+    console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+  },
+  getCheckboxProps: record => ({
+    disabled: record.name === 'Disabled User', // Column configuration not to be checked
+    name: record.name,
+  }),
+};
+function onChange(pagination, filters, sorter) {
+  console.log('params', pagination, filters, sorter);
+}
+class workerList extends Component {
+  state = {
+    filterDropdownVisible: false,
+    data,
+    searchText: '',
+    filtered: false,
+  };
+  onInputChange = (e) => {
+    this.setState({ searchText: e.target.value });
+  }
+  onSearch = () => {
+    const { searchText } = this.state;
+    const reg = new RegExp(searchText, 'gi');//g表示global全局搜索，i表示ignore care忽略大小写
+    this.setState({
+      filterDropdownVisible: false,
+      filtered: !!searchText,
+      data: data.map((record) => {
+        const match = record.name.match(reg);
+        if (!match) {
+          return null;
+        }
+        return {
+          ...record,
+          name: (
+            <span>
+              {record.name.split(reg).map((text, i) => (
+                i > 0 ? [<span className="highlight">{match[0]}</span>, text] : text
+              ))}
+            </span>
+          ),
+        };
+      }).filter(record => !!record),
+    });
+  }
+  render() {
+    const columns = [{
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      sorter: (a, b) => a.name.length - b.name.length,
+      filterDropdown: (
+        <div className="custom-filter-dropdown">
+          <Input
+            ref={ele => this.searchInput = ele}
+            placeholder="Search name"
+            value={this.state.searchText}
+            onChange={this.onInputChange}
+            onPressEnter={this.onSearch}
+          />
+          <Button type="primary" onClick={this.onSearch}>Search</Button>
+        </div>
+      ),
+      filterIcon: <Icon type="search" style={{ color: this.state.filtered ? '#108ee9' : '#aaa' }} />,
+      filterDropdownVisible: this.state.filterDropdownVisible,
+      onFilterDropdownVisibleChange: (visible) => {
+        this.setState({
+          filterDropdownVisible: visible,
+        }, () => this.searchInput && this.searchInput.focus());
+      },
     }, {
-      text: 'Jim',
-      value: 'Jim',
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+      sorter: (a, b) => a.age - b.age
     }, {
-      text: 'Submenu',
-      value: 'Submenu',
-      children: [{
-        text: 'Green',
-        value: 'Green',
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+      filters: [{
+        text: '筛选条件1',
+        value: '筛选条件1',
       }, {
-        text: 'Black',
-        value: 'Black',
+        text: '筛选条件2',
+        value: '筛选条件2',
       }],
-    }],
-    onFilter: (value, record) => record.name.indexOf(value) === 0,
-    sorter: (a, b) => a.name.length - b.name.length,
-  }, {
-    title: 'Age',
-    dataIndex: 'age',
-    defaultSortOrder: 'descend',
-    sorter: (a, b) => a.age - b.age,
-  }, {
-    title: 'Address',
-    dataIndex: 'address',
-    filters: [{
-      text: 'London',
-      value: 'London',
-    }, {
-      text: 'New York',
-      value: 'New York',
-    }],
-    filterMultiple: false,
-    onFilter: (value, record) => record.address.indexOf(value) === 0,
-    sorter: (a, b) => a.address.length - b.address.length,
-  }];
-  
-  const data = [];
-  for(let i=1;i<=100;i++){
-      data.push({
-          key:i,
-          name:`kayson ${i}`,
-          age:Math.ceil(Math.random()*10)+20,
-          address:`China ${i}`
-      })
+      onFilter: (value, record) => record.address.indexOf(value) === 0,
+      sorter: (a, b) => a.address.length - b.address.length
+    }];
+    return <Table rowSelection={rowSelection} columns={columns} dataSource={this.state.data} />;
   }
-  function onChange(pagination, filters, sorter) {
-    console.log('params', pagination, filters, sorter);
-  }
-
-const workerList = () => (
-    <Table columns={columns} dataSource={data} onChange={onChange}/>
-);
-
+}
 export default workerList;

5e07bf9c0a26b36c99df0f488e8d4220d89d08a7
kayson0512 <kayson0512@163.com>
2018-04-24 22:48:21 +0800

    custom analysis init

--- src/pages/customAnalysis/header.js
+++ src/pages/customAnalysis/header.js
@@ -1,14 +1,14 @@
 import React,{Component} from 'react'
-import { Menu, Icon, Layout, Dropdown } from 'antd'
+import { Menu, Icon, Layout, Dropdown, Button, Avatar } from 'antd'
 import './header.css'
 const { Header } = Layout
 const SubMenu = Menu.SubMenu;
 class Top extends Component{
     menu = {
-        nomalMenu:(
+        normalMenu:(
             <Menu>
                 <SubMenu title="柱形图">
-                    <Menu.Item>基本柱图</Menu.Item>
+                    <Menu.Item><Avatar shape="square" size="large" src="http://oudaz22af.bkt.clouddn.com/a1.jpg"></Avatar>基本柱图</Menu.Item>
                     <Menu.Item>水平基本柱图</Menu.Item>
                     <Menu.Item>梯形柱图</Menu.Item>
                     <Menu.Item>折现柱图</Menu.Item>
@@ -22,60 +22,60 @@ class Top extends Component{
                     <Menu.Item>基本饼图</Menu.Item>
                     <Menu.Item>多维度饼图</Menu.Item>
                 </SubMenu>
-                <SubMenu title="散点图" disabled>
+                <SubMenu title="散点图">
                     <Menu.Item>基本散点图</Menu.Item>
                     <Menu.Item>气泡图</Menu.Item>
                 </SubMenu>
             </Menu>
-          ),
-          mapMenu:(
+        ),
+        mapMenu:(
             <Menu>
                 <Menu.Item>3D平面基础地图</Menu.Item>
                 <Menu.Item>3D平面中国地图</Menu.Item>
                 <Menu.Item>3D球形地图</Menu.Item>
             </Menu>
-          ),
-          indexMenu:(
+        ),
+        indexMenu:(
             <Menu>
                 <Menu.Item>进度条</Menu.Item>
                 <Menu.Item>轮播图</Menu.Item>
                 <Menu.Item>状态卡片</Menu.Item>
                 <Menu.Item>文字标签</Menu.Item>
             </Menu>
-          ),
-          textMenu:(
+        ),
+        textMenu:(
             <Menu>
                 <Menu.Item>多行文本</Menu.Item>
                 <Menu.Item>通用标题</Menu.Item>
                 <Menu.Item>轮播列表</Menu.Item>
                 <Menu.Item>键值表格</Menu.Item>
             </Menu>
-          )
+        )
     };
     render() {
         return (
             <div className="Top">
-                <Header style={{ background: '#fff'}}>
-                    <Dropdown overlay={this.menu.normalMenu}>
+                <Header style={{ background: '#404040'}}>
+                    <Dropdown.Button overlay={this.menu.normalMenu}>
                         <a className="ant-dropdown-link">
-                        常规图表 <Icon type="down" />
+                        常规图表
                         </a>
-                    </Dropdown>
-                    <Dropdown overlay={this.menu.mapMenu}>
+                    </Dropdown.Button>
+                    <Dropdown.Button overlay={this.menu.mapMenu} style={{marginLeft:'20px'}}>
                         <a className="ant-dropdown-link">
-                        地图 <Icon type="down" />
+                        地图
                         </a>
-                    </Dropdown>
-                    <Dropdown overlay={this.menu.indexMenu}>
+                    </Dropdown.Button>
+                    <Dropdown.Button overlay={this.menu.indexMenu} style={{marginLeft:'20px'}}>
                         <a className="ant-dropdown-link">
-                        指标 <Icon type="down" />
+                        指标
                         </a>
-                    </Dropdown>
-                    <Dropdown overlay={this.menu.textMenu}>
+                    </Dropdown.Button>
+                    <Dropdown.Button overlay={this.menu.textMenu} style={{marginLeft:'20px'}}>
                         <a className="ant-dropdown-link">
-                        文字图表 <Icon type="down" />
+                        文字图表
                         </a>
-                    </Dropdown>
+                    </Dropdown.Button>
                 </Header>
             </div>
         );
--- src/pages/customAnalysis/index.js
+++ src/pages/customAnalysis/index.js
@@ -2,79 +2,19 @@ import React, { Component } from 'react';
 import {Icon,Layout,Menu} from 'antd';
 import {Link} from 'react-router-dom';
 import './index.css';
-// import Top from './header.js';
-// import Bottom from './footer.js';
-// import Contents from './content.js';
-import allMenu from '../../utils/menu';
+import Top from './header.js';
+import Bottom from './footer.js';
+import Contents from './content.js';
 const{Sider} = Layout;
 const SubMenu = Menu.SubMenu;
 class Container extends Component{
-	state={
-		current: 'index',
-		collapsed:false
-	};
-	componentDidMount() {
-    this.handleClick([], 'index')
-	};
-	handleClick = (e, special) => {
-    this.setState({
-      current: e.key || special,
-    });
-  };
-	toggle = () => {
-		this.setState({
-		  collapsed: !this.state.collapsed,
-		  mode: this.state.collapsed ? 'inline' : 'vertical'
-		});
-	};
-	clear = () => {
-    this.setState({
-      current: 'index',
-    });
-  };
 	render(){
 		return (
 				<Layout className="container">
-					<Sider 
-					trigger={null}
-					collapsible
-					collapsed={this.state.collapsed}
-					style={{backgroundColor:'#404040'}}
-					>
-						<Menu 
-						theme="dark" 
-						defaultSelectedKeys={['1']} 
-						mode="inline"
-						style={{backgroundColor:'#404040'}}
-						selectedKeys={[this.state.current]}
-						onClick={this.handleClick}
-						>
-						{
-							allMenu.map((subMenu) => {
-							  if (subMenu.children && subMenu.children.length) {
-								return (
-								  <SubMenu key={subMenu.url} title={<span><Icon type={subMenu.icon} /><span>{subMenu.name}</span></span>}>
-									{subMenu.children.map(menu => (
-									  <Menu.Item key={menu.url}><Link to={`/${menu.url}`}>{menu.name}</Link></Menu.Item>
-									))}
-								  </SubMenu>
-								)
-							  }
-							  return (
-								<Menu.Item key={subMenu.url}>
-								  <Link to={`/${subMenu.url}`}>
-									<Icon type={subMenu.icon} /><span className="nav-text">{subMenu.name}</span>
-								  </Link>
-								</Menu.Item>
-							  )
-							})
-						  }
-						</Menu>
-					</Sider>
+					<Top/>
 					<Layout>
-					{/*<Top toggle={this.toggle} collapsed={this.state.collapsed} clear={this.clear}/>
-					<Contents/>
-                        <Bottom/>*/}
+						<Contents/>  
+						<Bottom/>
 					</Layout>
 				</Layout>
 		)

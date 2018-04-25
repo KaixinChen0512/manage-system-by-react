3030b919ee467b2ac226f7bc04aa0f2945cdcafb
陈凯鑫 <kayson0512@chenkaixindeMacBook-Pro.local>
2018-04-25 21:48:27 +0800

    page instruction finished

--- src/pages/instruction/index.js
+++ src/pages/instruction/index.js
@@ -1,45 +1,40 @@
-/**
- * Created by hao.cheng on 2017/4/15.
- */
-import React from 'react';
-import { Row, Col, Card } from 'antd';
-const BasicTables = () => (
-    <div className="gutter-example">
-        <Row gutter={16}>
-            <Col className="gutter-row" md={24}>
-                <div className="gutter-box">
-                    <Card title="基础表格" bordered={false}>
-                       
-                    </Card>
-                </div>
-            </Col>
-        </Row>
-        <Row gutter={16}>
-            <Col className="gutter-row" md={24}>
-                <div className="gutter-box">
-                    <Card title="基础表格" bordered={false}>
-                    
-                    </Card>
-                </div>
-            </Col>
-        </Row>
-        <Row gutter={16}>
-            <Col className="gutter-row" md={12}>
-                <div className="gutter-box">
-                    <Card title="可控的筛选和排序" bordered={false}>
-                      
-                    </Card>
-                </div>
-            </Col>
-            <Col className="gutter-row" md={12}>
-                <div className="gutter-box">
-                    <Card title="自定义筛选" bordered={false}>
-                    
-                    </Card>
-                </div>
-            </Col>
-        </Row>
-    </div>
-);
+import React from 'react'
+import './index.css'
 
-export default BasicTables;
+export default class follow extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            col:'#666'
+        }
+    }
+
+    getRandomColor = () => {
+        this.setState({
+            col: '#'+('00000'+(Math.random()*0x1000000<<0).toString(16)).slice(-6)
+        });
+    }
+
+    // 组件渲染后，500毫秒改变一次组件颜色
+    componentDidMount() {
+        this.interval = setInterval(this.getRandomColor, 500);
+    }
+
+    // 组件将要死亡时清除计时器
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
+        const { col } = this.state
+
+        return (
+            <div className="animated flip ani-box">
+                <div><a href="https://github.com/KaixinChen0512" className="welcome animated flip text" style={{ color: col }}>项目地址</a></div>
+                <img src="http://oudaz22af.bkt.clouddn.com/face.png" width="300" alt="logo" className="lastPic" />
+                <div className="animated swing discribe">本系统主要致力于高效化管道工程管理</div>
+                <div className="animated swing discribe">项目源码请访问<a href="https://github.com/KaixinChen0512">我的GitHub</a>。欢迎 <a href="http://chenkaixin.cn">访问我的个人主页!</a></div>
+            </div>
+        )
+    }
+}

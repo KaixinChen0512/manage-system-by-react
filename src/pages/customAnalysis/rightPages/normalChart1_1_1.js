96fce0ed37eb505aaaa80bf09a7438e350d1e2b0
kayson0512 <kayson0512@163.com>
2018-05-03 14:25:03 +0800

    图表交互选择功能-可视化空间 添加完毕

--- src/pages/customAnalysis/rightPages/normalChart1_1_1.js
+++ src/pages/customAnalysis/rightPages/normalChart1_1_1.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import {List,Tabs,Icon,Card,Button,Form,Input,Checkbox,Collapse,Select,Row,Col,Slider,InputNumber,Divider,Modal,Upload,message} from 'antd';
+import {List,Tabs,Icon,Card,Button,Form,Input,Checkbox,Collapse,Select,Row,Col,Slider,InputNumber,Divider,Modal,Upload,message,Switch} from 'antd';
 import { Scrollbars} from 'react-custom-scrollbars';
 import './normalChart1_1_1.css';
 
@@ -415,49 +415,43 @@ class formNormalChart1_1_1 extends Component{
                         </Card>
                     </TabPane>
                     <TabPane tab={<span><Icon type="tool" />交互</span>} key="3">
-                    <Card title="基本柱图" style={{ width: '100%',height:'550px' }}>
+                    <Card title="添加自定义控件" style={{ width: '100%',height:'550px' }}>
                     <Form onSubmit={this.handleSubmit} className="login-form">
                         <FormItem>
-                            <Scrollbars style={{ width: '350px', height: '390px' }}>
                                 <Collapse bordered={false} defaultActiveKey={['1']}>
-                                    <Panel header="全局样式" key="1">
-                                        <FormItem
-                                            labelCol={{ span: 6 }}
-                                            wrapperCol={{ span: 18 }}
-                                            label={(
-                                                <span>
-                                                    字体
-                                        </span>
-                                            )}
-                                        >
-                                            {getFieldDecorator('字体', {
-                                                initialValue: '微软雅黑'
-                                            })(
-                                                <Select style={{ width: 120 }}>
-                                                    <Option value="微软雅黑">微软雅黑</Option>
-                                                    <Option value="宋体">宋体</Option>
-                                                    <Option value="黑体" >黑体</Option>
-                                                    <Option value="隶书">隶书</Option>
-                                                </Select>
-                                            )}
+                                    <Panel header="可视化控件" key="1">
+                                        <FormItem>
+                                            <Row>
+                                                <Col span={12}>
+                                                    坐标轴指示器
+                                                </Col>
+                                                <Col span={12}>
+                                                    <Switch checkedChildren="开" unCheckedChildren="关" defaultChecked />
+                                                </Col>
+                                            </Row>
                                         </FormItem>
-                                        <FormItem
-                                            labelCol={{ span: 6 }}
-                                            wrapperCol={{ span: 18 }}
-                                            label={(
-                                                <span>
-                                                    背景颜色
-                                        </span>
-                                            )}
-                                        >
-                                            {getFieldDecorator('背景颜色', {
-                                                initialValue: '#FFFFFF'
-                                            })(
-                                                <Input placeholder="请输入十六进制颜色码" />
-                                            )}
+                                        <FormItem>
+                                            <Row>
+                                                <Col span={12}>
+                                                    数据筛选
+                                                </Col>
+                                                <Col span={12}>
+                                                    <Switch checkedChildren="开" unCheckedChildren="关" defaultChecked />
+                                                </Col>
+                                            </Row>
+                                        </FormItem>
+                                        <FormItem>
+                                            <Row>
+                                                <Col span={12}>
+                                                    参数回调
+                                                </Col>
+                                                <Col span={12}>
+                                                    <Switch checkedChildren="开" unCheckedChildren="关" defaultChecked />
+                                                </Col>
+                                            </Row>
                                         </FormItem>
                                     </Panel>
-                                    <Panel header="X轴" key="2">
+                                    <Panel header="算法" key="2">
                                         <FormItem
                                             labelCol={{ span: 6 }}
                                             wrapperCol={{ span: 18 }}
@@ -502,93 +496,7 @@ class formNormalChart1_1_1 extends Component{
                                             )}
                                         </FormItem>
                                     </Panel>
-                                    <Panel header="Y轴" key="3">
-                                        <FormItem
-                                            labelCol={{ span: 6 }}
-                                            wrapperCol={{ span: 18 }}
-                                            label={(
-                                                <span>
-                                                    文本颜色
-                                            </span>
-                                            )}
-                                        >
-                                            {getFieldDecorator('Y轴文本颜色', {
-                                                initialValue: '#333333'
-                                            })(
-                                                <Input placeholder="请输入十六进制颜色码" />
-                                            )}
-                                        </FormItem>
-                                        <FormItem
-                                            labelCol={{ span: 6 }}
-                                            wrapperCol={{ span: 18 }}
-                                            label={(
-                                                <span>
-                                                    文本字号
-                                            </span>
-                                            )}
-                                        >
-                                            {getFieldDecorator('Y轴文本字号', {
-                                                initialValue: this.state.YFontSize
-                                            })(
-                                                <Row>
-                                                    <Col span={12}>
-                                                        <Slider min={10} max={100} onChange={this.changeYFontSize} value={this.state.YFontSize} />
-                                                    </Col>
-                                                    <Col span={4}>
-                                                        <InputNumber
-                                                            min={10}
-                                                            max={100}
-                                                            style={{ marginLeft: 16 }}
-                                                            value={this.state.YFontSize}
-                                                            onChange={this.changeYFontSize}
-                                                        />
-                                                    </Col>
-                                                </Row>
-                                            )}
-                                        </FormItem>
-                                    </Panel>
-                                    <Panel header="图表尺寸" key="4">
-                                        <FormItem
-                                            labelCol={{ span: 6 }}
-                                            wrapperCol={{ span: 18 }}
-                                            label={(
-                                                <span>
-                                                    宽度
-                                            </span>
-                                            )}
-                                        >
-                                            {getFieldDecorator('图表宽度', {
-                                                initialValue: '500'
-                                            })(
-                                                <InputNumber
-                                                    min={0}
-                                                    max={600}
-                                                    style={{ marginLeft: 16 }}
-                                                />
-                                            )}
-                                        </FormItem>
-                                        <FormItem
-                                            labelCol={{ span: 6 }}
-                                            wrapperCol={{ span: 18 }}
-                                            label={(
-                                                <span>
-                                                    高度
-                                            </span>
-                                            )}
-                                        >
-                                            {getFieldDecorator('图表高度', {
-                                                initialValue: '500'
-                                            })(
-                                                <InputNumber
-                                                    min={0}
-                                                    max={600}
-                                                    style={{ marginLeft: 16 }}
-                                                />
-                                            )}
-                                        </FormItem>
-                                    </Panel>
                                 </Collapse>
-                            </Scrollbars>
                         </FormItem>
                         <FormItem>
                             <Button type="primary" htmlType="submit" style={{marginLeft:'110px'}}>确认修改</Button>
